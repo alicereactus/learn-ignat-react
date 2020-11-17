828e30ae019eebe6d5704d693fcee3c53fa125e7
alicereactus <agamia@mail.ru>
2020-11-17 19:05:57 +0400

    Updates

---
+++ precache-manifest.a0e8d9f112a979ac125cfd47db49aacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee7c892c9a56f3868d62ba92bc205b6",
+    "url": "/learn-ignat-react/index.html"
+  },
+  {
+    "revision": "dc98baa099eb2978a155",
+    "url": "/learn-ignat-react/static/css/main.516bc699.chunk.css"
+  },
+  {
+    "revision": "3f6594a9239600f6f804",
+    "url": "/learn-ignat-react/static/js/2.dd097942.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learn-ignat-react/static/js/2.dd097942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc98baa099eb2978a155",
+    "url": "/learn-ignat-react/static/js/main.4b28c58e.chunk.js"
+  },
+  {
+    "revision": "e4d2a33b01fe78b9d7aa",
+    "url": "/learn-ignat-react/static/js/runtime-main.81821f28.js"
+  }
+]);
